6e0fae8a0e567339831b5ba1ec7e0f5d94f4f08e
Andres Lederer <dobrandres@gmail.com>
2020-04-09 20:47:06 -0300

    cambio los valores del cilindro

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Circulo circulo1 = new Circulo(2,"azul");
-		Circulo cilindro1 = new Cilindro(3,2,"verde");
+		Circulo cilindro1 = new Cilindro(28,0.5,"amarillo");
 	
 		System.out.println("CIRCULO: "+circulo1.toString());
 		System.out.println("CILINDRO: "+cilindro1.toString());
